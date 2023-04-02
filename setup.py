01a8680ae8bd46e364f486d89899565b0945cbe5
NickVincent_Cagliuso <nph4zk@virginia.edu>
2023-04-01 22:09:15 -0400

    Added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='BookLover',
+      version='0.1',
+      description='A package for recording books read by readers',
+      url='https://github.com/n-cagliuso/BookLoverHW',
+      author='Nick Cagliuso',
+      author_email='nph4zk@virginia.edu',
+      license='Creative Commons',
+      packages=['BookLover'])
